7630aa3dda594487de66000d4939a0c77087ae78
Asmath Jabeen Shaik <asmath9696@gmail.com>
2024-08-14 10:49:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to submit button
+document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    // Get form data
+})
